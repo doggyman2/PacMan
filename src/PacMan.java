43544b9ad8881470b1a5401bf9a178de439d7e31
doggyman2 <shuta1217@yahoo.co.jp>
2025-01-12 23:55:38 +0900

    Add files via upload

--- src/PacMan.java
+++ src/PacMan.java
@@ -5,6 +5,28 @@ import java.util.random.*;
 import javax.swing.*;
 
 public class PacMan extends JPanel {
+
+class Block{
+    int x;
+    int y;
+    int width;
+    int height;
+    Image image;
+
+    int starx;
+    int starty;
+
+    Block(Image image,int x, int y,int width, int height){
+        this.image=image;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.startx=x;
+        this.starty=y;
+
+    }
+}
     private int rowCount =21;
     private int columnCount =19;
     private int tileSize = 32;
@@ -22,6 +44,11 @@ public class PacMan extends JPanel {
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
 
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghost;
+    Block pacman;
+
     PacMan(){
         setPreferredSize(new Dimension(boardWidth, bordHeight));
         setBackground(Color.black);
@@ -37,7 +64,7 @@ public class PacMan extends JPanel {
         pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
         pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
         pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
-        
+
     }
 
 }
